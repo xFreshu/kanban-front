b13441c2e6241692ba227402e7de22ab92a4449f
xFreshu <lprzybyszcontact@gmail.com>
2023-02-10 12:29:19 +0100

    create basic layout for board

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import classes from "./Board.module.scss";
 
 const Board = () => {
-  return <div className={classes.container}></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.column}>
+        <span>TO DO</span>
+        <div className={classes.task}>
+          <span>TITLE</span>
+          <span>0 of 3 substasks</span>
+          <div>Priority: low</div>
+        </div>
+      </div>
+      <div className={`${classes.column} ${classes.addColumn}`}>
+        <div>ADD NEW COLUMN</div>
+      </div>
+    </div>
+  );
 };
 
 export default Board;
